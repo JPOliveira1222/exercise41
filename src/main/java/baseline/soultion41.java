680cf71398954256d1b853e92b692e97eb0b199f
JP Oliveira <joaopedro.costa.oliveira@gmail.com>
2021-10-15 23:45:36 -0400

    Pseudo code for exercise 41

---
+++ src/main/java/baseline/soultion41.java
@@ -0,0 +1,30 @@
+package baseline;
+
+public class soultion41 {
+
+    /*
+        Define the name sorter
+        read from a scanner and put into array list
+        add the names scanned to the list of names
+
+        print the names in the files
+        Create a file writing function for output
+
+        write out header and output how many names there are in the file
+
+        write the name of the files after the header a separator
+        close out the file writing function
+
+
+        create a scanner object that will read from input file
+        declare arraylist for storing names
+        read names from the input file by calling the function that reads names
+        using the sort function sort the list of names
+        close the scanner object
+
+        write out the names
+        have an exception in case of an error. 
+     */
+
+
+}
